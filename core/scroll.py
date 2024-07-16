1e1da05e345d13b4dc5770bc54a0617a67dad431
tarik <4taras4@gmail.com>
2024-07-16 16:03:27 +0300

    added scroll

---
+++ core/scroll.py
@@ -0,0 +1,9 @@
+from core.client import WebClient
+
+class ScrollCanvas(WebClient):
+    
+    def __init__(self, id:int, key: str):
+        self.__init__(id, key, 'scroll')
+
+
+# https://canvas.scroll.cat/badge/check?badge=0x3dacAd961e5e2de850F5E027c70b56b5Afa5DfeD&recipient=0x387c6e7fe88042966aCF9EAc1852E0F101E4b37D
