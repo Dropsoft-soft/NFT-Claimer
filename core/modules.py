a8d6df2d620fde08029f704924dee5fd483fc2c8
tarik <4taras4@gmail.com>
2024-08-01 12:08:49 +0300

    Added random NFT mint

--- core/modules.py
+++ core/modules.py
@@ -3,6 +3,9 @@ from core.baseSummer import BaseSummer
 from core.beranft import BeraTestnet
 from core.scroll import ScrollCanvas
 from core.client import WebClient
+from user_data.config import MINT_RANDOM_SUMMER_BASE_NFT
+import random
+from loguru import logger
 
 async def linea_layer3_nft(account_id, key):
     web3 = WebClient(
@@ -20,14 +23,21 @@ async def base_summer_nft(account_id, key):
     web3 = BaseSummer(
         account_id, key
     )
-    await web3.mint_nft()
-    await web3.mint_sanfrancisco_nft()
-    await web3.mint_birthday_toshi_nft()
-    await web3.mint_summer_chibling_nft()
-    await web3.mint_eth_cant_be_stopped_nft()
-    await web3.mint_midnight_diner_pass_nft()
-    await web3.mint_doodlestv_pass_nft()
-    await web3.mint_onchain_summer_board_nft()
+
+    if MINT_RANDOM_SUMMER_BASE_NFT:
+        array_nft = [web3.mint_nft, web3.mint_sanfrancisco_nft, web3.mint_birthday_toshi_nft, web3.mint_summer_chibling_nft, web3.mint_eth_cant_be_stopped_nft, web3.mint_midnight_diner_pass_nft, web3.mint_doodlestv_pass_nft, web3.mint_onchain_summer_board_nft]
+        selected = random.choice(list(array_nft))
+        logger.info(f'Mint random nft: {selected}')
+        await selected()
+    else:
+        await web3.mint_nft()
+        await web3.mint_sanfrancisco_nft()
+        await web3.mint_birthday_toshi_nft()
+        await web3.mint_summer_chibling_nft()
+        await web3.mint_eth_cant_be_stopped_nft()
+        await web3.mint_midnight_diner_pass_nft()
+        await web3.mint_doodlestv_pass_nft()
+        await web3.mint_onchain_summer_board_nft()
 
 async def bera_nft(account_id, key):
     web3 = BeraTestnet(
--- user_data/config.py
+++ user_data/config.py
@@ -5,6 +5,7 @@ DELAY_TO = 120
 USE_ENCRYPTED_WALLETS = False
 ENCRYPTED_PASSWORD = ""
 FEE_MULTIPLIER = 1.1
+MINT_RANDOM_SUMMER_BASE_NFT = False # If true will mint 1 random nfr instead of full list
 
 # Scroll sessions nickname to register list
 MINT_RANDOM_NICKNAME = [
