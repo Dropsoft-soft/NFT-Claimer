e9101b42578f6a33a7530ffd37c649859847875b
tarik <4taras4@gmail.com>
2024-08-06 20:18:32 +0300

    Fixes for badges

--- core/scroll.py
+++ core/scroll.py
@@ -7,7 +7,7 @@ from loguru import logger
 import asyncio, random
 from random_user_agent.user_agent import UserAgent
 from core.request import global_request
-from core.utils import BADGE_LIST, WALLET_PROXIES, intToDecimal
+from core.utils import BADGE_LIST, WALLET_PROXIES, intToDecimal, sleep
 from user_data.config import FEE_MULTIPLIER, USE_PROXY
 from user_data.config import MINT_RANDOM_NICKNAME
 user_agent_rotator = UserAgent(software_names=['chrome'], operating_systems=['windows', 'linux'])
@@ -105,14 +105,15 @@ class ScrollCanvas(WebClient):
                 'data': data,
                 'nonce': await self.web3.eth.get_transaction_count(self.address),
                 'from': self.address,
-                'gasPrice': await self.web3.eth.gas_price,
+                'maxFeePerGas': int(await self.web3.eth.gas_price*FEE_MULTIPLIER),
+                'maxPriorityFeePerGas': await self.web3.eth.max_priority_fee,                
                 'gas': 0,
                 'chainId': self.chain_id,
                 'to': to,
                 'value': 0,
             }
             gas = await self.web3.eth.estimate_gas(contract_txn)
-            contract_txn['gas'] = int(gas*1.05)
+            contract_txn['maxFeePerGas'] = int(gas*1.05)
             status, tx_link = await self.send_tx(contract_txn)
             if status == 1:
                 logger.success(f"[{self.id}] {self.address} | claim nft | {tx_link}")
@@ -128,6 +129,7 @@ class ScrollCanvas(WebClient):
     async def mint_all_available_badge(self):
         badge_array = BADGE_LIST['badges']
         logger.info(f'Received badges: {len(badge_array)}')
+        minted_counter = 0
         for jsonBadge in badge_array:
             badge = jsonBadge['badgeContract']
             domain = jsonBadge['baseUrl']
@@ -136,19 +138,62 @@ class ScrollCanvas(WebClient):
             is_elligable = json['eligibility']
             logger.info(f'[{self.id}] Eligable for mint {name}: {is_elligable}')
             if is_elligable == True:
-                await asyncio.sleep(5)
+                await sleep(5, 30)
                 get_tx_data = await self.get_tx_for_badge(domain, badge)
-                await asyncio.sleep(5)
+                logger.info('Get transaction data')
+                await sleep(5, 30)
                 minted_badge = await self.mintFromJSON(get_tx_data)
-                await asyncio.sleep(5)
                 if minted_badge:
                     logger.success(f'[{self.id}] Badge: {badge} minted')
+                    minted_counter += 1
                 else:
                     logger.info(f'[{self.id}] Badge: {badge} not minted')
             else: 
                 logger.info(f'[{self.id}] Badge: {badge} user not elligable for mint')
-
+        logger.info(f'[{self.id} - {self.address}] Minted {minted_counter}')
         
 # 0xC47300428b6AD2c7D03BB76D05A176058b47E6B0
 # 0x39fb5E85C7713657c2D9E869E974FF1e0B06F20C
 # https://canvas.scroll.cat/badge/check?badge=0x3dacAd961e5e2de850F5E027c70b56b5Afa5DfeD&recipient=
+
+# // {
+# //     "name": "Scroll Overlord",
+# //     "image": "https://xenobunny.s3.amazonaws.com/images/scroll-canvas/scroll-overlord-badge.png",
+# //     "description": "Participated in at least one Land War season on scroll.",
+# //     "badgeContract": "0xb2c69173829E23EE2876FBe13Ab474FEe101bc64",
+# //     "category": "Achievement",
+# //     "issuer": {
+# //         "name": "XenoBunny",
+# //         "logo": "https://scroll-eco-list.netlify.app/logos/XenoBunny.jpg",
+# //         "origin": "https://www.xenobunny.xyz/"
+# //     },
+# //     "native": false,
+# //     "eligibilityCheck": true
+# // },
+# // {
+# //     "name": "Scroll Domain Master Badge 🎖️",
+# //     "image": "https://assets.znsconnect.io/domain-master-badge.png",
+# //     "description": "Achieve the Scroll Domain Master Badge by minting 5 domains on Scroll. This prestigious badge marks you as a master in the domain space! Mint your domains now: https://zns.bio/",
+# //     "badgeContract": "0x55B867a955e4384bcAc03eF7F2E492F68016C152",
+# //     "category": "Achievement",
+# //     "issuer": {
+# //         "name": "ZNS Connect",
+# //         "logo": "https://scroll-eco-list.netlify.app/logos/ZNS Connect.jpg",
+# //         "origin": "https://www.znsconnect.io/"
+# //     },
+# //     "native": false,
+# //     "eligibilityCheck": true
+# // },
+# //         {
+# //     "name": "ZNS Badge",
+# //     "image": "https://assets.znsconnect.io/scroll-badge.jpg",
+# //     "description": "Earn this badge for minting a ZNS Domain on Scroll",
+# //     "badgeContract": "0x0246D65bA41Da3DB6dB55e489146eB25ca3634E5",
+# //     "issuer": {
+# //         "name": "ZNS Connect",
+# //         "logo": "https://scroll-eco-list.netlify.app/logos/ZNS Connect.jpg",
+# //         "origin": "https://www.znsconnect.io/"
+# //     },
+# //     "eligibilityCheck": true,
+# //     "native": false
+# // },
--- core/utils.py
+++ core/utils.py
@@ -8,6 +8,7 @@ import sys
 import os
 import json
 from user_data.config import ENCRYPTED_PASSWORD, USE_ENCRYPTED_WALLETS
+from loguru import logger
 
 def decrypt_string(encrypted_text, password, salt):
     encrypted_text = bytes.fromhex(encrypted_text).decode('utf-8')
@@ -70,4 +71,12 @@ async def async_sleeping(from_sleep, to_sleep):
     for i in tqdm(range(x), desc='sleep ', bar_format='{desc}: {n_fmt}/{total_fmt}'):
         await asyncio.sleep(1)
 
+
+
+async def sleep(sleep_from: int, sleep_to: int):
+    delay = random.randint(sleep_from, sleep_to)
+
+    logger.info(f"💤 Sleep {delay} s.")
+    for _ in range(delay):
+        await asyncio.sleep(1)
 WALLET_PROXIES  = get_wallet_proxies(WALLETS, PROXIES)
